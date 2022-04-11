be756d58069d85d5625c735d8a009f5d6e2a1148
SORRENTINO Alma <alma.sorrentino@igtp3-pc07.isim.intra>
2022-04-11 09:11:42 +0200

    fonctions envoi et réception +vérif lancement

--- client.c
+++ client.c
@@ -7,8 +7,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+void envoi(int dS) {
+    printf("Entrez un message\n");
+    char * m = (char *) malloc(30*sizeof(char));
+    fgets( m, 30*sizeof(char), stdin ); 
+    size_t len= strlen(m)+1;
+    int snd2 = send(dS, &len, sizeof(len),0);
+    if (snd2 == -1){perror("Erreur envoi taille message");}
+    int snd = send(dS, m, len , 0) ;
+    if (snd == -1){perror("Erreur envoi message");}
+    printf("Message Envoyé \n");
+}
+
+void reception(int dS)
+  int r;
+  ssize_t rcv = recv(dS, &r, sizeof(int), 0) ;
+  if (rcv == -1){perror("Erreur réception message");};
+  printf("Réponse reçue : %d\n", r) ;
+  }
+}
+
 int main(int argc, char *argv[]) {
+
+    if(argc != 4){
+    printf("lancement : ./client IPServeur port numéroClient\n");
+    exit(1);
+  }
+
   printf("Début programme\n");
+
 int dS = socket(PF_INET, SOCK_STREAM, 0);
 if (dS == -1){
   perror("Erreur dans la création de la socket");
@@ -21,28 +48,29 @@ aS.sin_family = AF_INET;
 inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
 aS.sin_port = htons(atoi(argv[2])) ;
 socklen_t lgA = sizeof(struct sockaddr_in) ;
+int num = atoi(argv[3]);
+if (numm != 1 || num != 2) {
+  perror("Argument numéro client invalide");
+  exit(0);
+}
 int co = connect(dS, (struct sockaddr *) &aS, lgA) ;
 if (co == -1){
   perror("Erreur envoi demmande de connexion");
   exit(0);}
-
 printf("Socket Connecté\n");
-while(1){
-  printf("Entrez un message\n");
-  char * m = (char *) malloc(30*sizeof(char));
-  fgets( m, 30*sizeof(char), stdin ); 
-  size_t len= strlen(m)+1;
-  int snd2 = send(dS, &len, sizeof(len),0);
-  if (snd2 == -1){perror("Erreur envoi taille message");}
-  int snd = send(dS, m, len , 0) ;
-  if (snd == -1){perror("Erreur envoi message");}
-  printf("Message Envoyé \n");
 
-  int r;
-  ssize_t rcv = recv(dS, &r, sizeof(int), 0) ;
-  if (rcv == -1){perror("Erreur réception message");};
-  printf("Réponse reçue : %d\n", r) ;
+
+while(1){
+  if (num == 1) {
+    envoi(dS);
+    reception(dS);
   }
+  else {
+    reception(dS);
+    envoi(dS);
+  }
+  
+
 int sd = shutdown(dS,2) ;
 if (sd == -1){perror("Erreur shutdown");}
 printf("Fin du programme");
