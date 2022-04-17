7c1a66fcb248a1fc3f4297c0de29eb2b87c11c42
DisaroSkell <Hydralisk34980@gmail.com>
2022-04-17 17:57:29 +0200

    Modifictions du traitement serveur, l'échange de messages fonctionne (mais plante parfois)

--- fonctions.c
+++ fonctions.c
@@ -36,17 +36,19 @@ void* traitement_serveur(void * paramspointer){
 
     struct traitement_params * params = paramspointer;
 
+    int numclient = params->numclient;
+
     size_t len = 0;
-    printf("Client socket = %d\n", params->clienttab[params->numclient]);
+    printf("%d: Client socket = %d\n", numclient, params->clienttab[numclient]);
     while (1) {
-        ssize_t rcv_len = recv(params->clienttab[params->numclient], &len, sizeof(len), 0);
+        ssize_t rcv_len = recv(params->clienttab[numclient], &len, sizeof(len), 0);
         if (rcv_len == -1) perror("Erreur réception taille message");
-        printf("Longueur du message reçu: %d\n",(int)len);
+        printf("%d: Longueur du message reçu: %d\n", numclient, (int)len);
         char * msg = (char *) malloc((len)*sizeof(char));
-        ssize_t rcv = recv(params->clienttab[params->numclient], msg, len, 0);
+        ssize_t rcv = recv(params->clienttab[numclient], msg, len, 0);
         if (rcv == -1) perror("Erreur réception message");
         else {
-            printf("Message reçu : %s\n", msg);
+            printf("%d: Message reçu : %s\n", numclient, msg);
         }
 
         int end = strcmp(msg, "fin\n");
@@ -54,21 +56,20 @@ void* traitement_serveur(void * paramspointer){
         if (end == 0) {
             char * messagedeco = "Déconnexion en cours...\n";
             int tailledeco = strlen(messagedeco);
-            int senddecotaille = send(params->clienttab[params->numclient], &tailledeco, sizeof(tailledeco), 0);
-            int senddeco = send(params->clienttab[params->numclient], messagedeco, tailledeco, 0);
-            printf("Fin du thread\n");
+            int senddecotaille = send(params->clienttab[numclient], &tailledeco, sizeof(tailledeco), 0);
+            int senddeco = send(params->clienttab[numclient], messagedeco, tailledeco, 0);
+            printf("%d: Fin du thread\n", numclient);
             pthread_exit(0);
         }
 
-        printf("Pour rappel, je suis le thread en charge du client %d\n", params->numclient);
         for(int i; i<=100; i++){
-            if (params->clienttab[i] != params->clienttab[params->numclient] && params->clienttab[i] != 0) {
-                printf("client = %d\n", params->clienttab[i]);
+            if (params->clienttab[i] != params->clienttab[numclient] && params->clienttab[i] != 0) {
+                printf("%d: client = %d\n", numclient, params->clienttab[i]);
                 int snd2 = send(params->clienttab[i], &len, sizeof(len), 0);
                 if (snd2 == -1) perror("Erreur envoi taille message");
                 int snd = send(params->clienttab[i], msg, len , 0);
                 if (snd == -1) perror("Erreur envoi message");
-                printf("Message Envoyé\n");
+                printf("%d: Message Envoyé au client %d: %s\n", numclient, params->clienttab[i], msg);
             }
         }
     }
