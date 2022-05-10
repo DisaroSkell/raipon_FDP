b64e6b05c77eb168f751cb1061f8244ed700d111
DisaroSkell <Hydralisk34980@gmail.com>
2022-05-10 10:26:53 +0200

    Documentation des fonctions dans les fichiers fonctions_*.h

--- Client/fonctions_client.h
+++ Client/fonctions_client.h
@@ -1,18 +1,38 @@
 #define taille_pseudo 30 // taille du pseudo en comptant le \0
 
+/**
+ * @brief Structure qui sera passée au thread de réception client
+ */
 typedef struct argsrec {
   int socket;
   int fin; // Pointer sur booléen, vrai ssi le programme doit se terminer
 }argsrec;
 
-// Le thread de reception de messages du client
+/**
+ * @brief Le thread de reception de messages du client
+ * 
+ * @param argpointer Pointeur vers une structure (on utilisera argsrec)
+ */
 void * thread_reception(void * argpointer);
 
-// Lecture des messages dans le terminal
+/**
+ * @brief Lecture des messages dans le terminal puis envoi
+ * 
+ * @param dS Socket du serveur pour l'envoi
+ * @return false si le message saisie est "fin\n", true sinon (utilisé pour l'arrêt)
+ */
 int lecture_message(int dS);
 
-// Envoi d'un message par le socket (envoi de la taille puis du message)
+/**
+ * @brief Envoi d'un message par le socket (envoi de la taille puis du message)
+ * 
+ * @param socket Socket du destinataire
+ * @param msg Message à envoyer
+ * @return 0 si tout se passe bien, -1 si erreur d'envoi
+ */
 int envoi_message(int socket, char * msg);
 
-// Fonction de signal CTRL C du client
+/**
+ * @brief Fonction de signal CTRL C client
+ */
 void signal_handleCli(int sig);
--- Serveur/fonctions_serveur.h
+++ Serveur/fonctions_serveur.h
@@ -1,15 +1,24 @@
-#define nb_client_max 3
+#define nb_client_max 3 // Nombre de client maximum pouvant être accueillis sur la messagerie en simultané
 #define taille_pseudo 30 // taille du pseudo en comptant le \0
 
+/**
+ * @brief Structure pour un client (socket et pseudo)
+ */
 typedef struct client {
   int socket;
   char * pseudo;
 }client;
 
+/**
+ * @brief Structure pour le thread de relai de message pour chaque client
+ */
 typedef struct traitement_params {
   int numclient;
 }traitement_params;
 
+/**
+ * @brief Structure pour une commande: id, nom, message, destinataire (dans le cadre du mp)
+ */
 typedef struct commande {
   // L'id de la commande: -1 = commande / non reconnue; 0 = pas de commande; 1 = "/";
   int id_op;
@@ -18,34 +27,74 @@ typedef struct commande {
   char * user;
 }commande;
 
-// Envoi d'un message par le socket (envoi de la taille puis du message)
+/**
+ * @brief Envoi d'un message par le socket (envoi de la taille puis du message)
+ * 
+ * @param socket Socket du destinataire
+ * @param msg Message à envoyer
+ * @return 0 si tout se passe bien, -1 si erreur d'envoi
+ */
 int envoi_message(int socket, char * msg);
 
-// Thread de traitement serveur, un thead par client
+/**
+ * @brief Thread de traitement serveur, un thead par client
+ * 
+ * @param paramspointer Pointeur sur la structure de paramètres (traitement_params)
+ */
 void * traitement_serveur(void * paramspointer);
 
-// Envoi d'un message à un utilisateur par son indice dans le tableau clients
-// Envoi de la taille, puis du message
-// Forme du message : "<pseudo> : message"
+/**
+ * @brief Envoi d'un message à un utilisateur par son indice dans le tableau clients
+ * Envoi de la taille, puis du message
+ * Forme du message : "<pseudo> : message"
+ * 
+ * @param numreceveur Indice du receveur dans le tableau clients
+ * @param msg Message à envoyer
+ * @param envoyeur Pseudo de l'envoyeur
+ * @return 0 si tout se passe bien, -1 si erreur d'envoi
+ */
 int envoi_direct(int numreceveur, char * msg, char * envoyeur);
 
-// Reception d'un message du client numclient dans le tableau clients
-// Reception de la taille, puis du message
+/**
+ * @brief Reception d'un message du client numclient dans le tableau clients
+ * Reception de la taille, puis du message
+ * 
+ * @param numclient Indice du client dans le tableau
+ * @return Le message reçu
+ */
 char * reception_message(int numclient);
 
-// Cherche un client dans le tableau clients
-// Renvoie: -1 si non trouvé, l'index dans le tableau clients sinon.
+/**
+ * @brief Cherche un client dans le tableau clients par son pseudo
+ * 
+ * @param pseudo Pseudo de la personne recherchée
+ * @return -1 si non trouvé, l'index dans le tableau clients sinon.
+ */
 int chercher_client(char * pseudo);
 
-// Cherche une place dans le tableau clients
-// Renvoie: l'index de la première place trouvée, la taille du tableau si aucune place n'est trouvée
+/**
+ * @brief Cherche une place dans le tableau clients
+ * 
+ * @return Si aucune place n'est trouvée la taille du tableau, sinon l'index de la première place trouvée
+ */
 int chercher_place();
 
-// Renvoie le manuel lu dans Public/manuel sous forme de String
+/**
+ * @brief Consulte le fichier Public/manuel
+ * 
+ * @return Le manuel lu en String
+ */
 char * lire_manuel();
 
-// Renvoie un objet commande, cf la struct pour plus d'info de retour
+/**
+ * @brief Analyse le message pour y reconnaître une commande
+ * 
+ * @param msg Message à analyser
+ * @return Un objet commande, cf la struct pour plus d'info de retour
+ */
 commande gestion_commande(char * msg);
 
-// Gestion du signal CTRL C serveur
+/**
+ * @brief Gestion du signal CTRL C serveur
+ */
 void signal_handle(int sig);
